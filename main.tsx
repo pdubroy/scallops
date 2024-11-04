63248d8a74e546f70febfb2933462e4580b5b595
Justin Bennett <git@just-be.dev>
2024-11-03 19:57:23 -0500

    Add pico css
    This isn't intended to be the ultimate solution, but while we're iterating this gives us some styles that look good enough.

--- main.tsx
+++ main.tsx
@@ -47,9 +47,18 @@ const Layout: FC = ({ children }) => {
       {html`<!DOCTYPE html>`}
       <html lang="en">
         <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="color-scheme" content="light dark" />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+          />
           <title>Scallops</title>
         </head>
-        <body>{children}</body>
+        <body>
+          <main class="container">{children}</main>
+        </body>
       </html>
     </>
   );
